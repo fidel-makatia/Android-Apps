12e8117db78c93887338537caf87bcbc2ac230f7
Fidel Makatia <fidelmakatia@gmail.com>
2022-02-07 14:48:04 +0300

    Second

--- FadingAnimatons/app/src/main/java/com/example/fadinganimatons/MainActivity.java
+++ FadingAnimatons/app/src/main/java/com/example/fadinganimatons/MainActivity.java
@@ -7,26 +7,24 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    int count=0;
+
     public void fade(View view){
-        ImageView imageView=(ImageView) findViewById(R.id.imageView);
+
         ImageView imageView1=(ImageView)findViewById(R.id.homerimageView);
-        if(count==0) {
-            imageView.animate().alpha(0).setDuration(2000);
-            imageView1.animate().alpha(1).setDuration(2000);
-            count++;
-        }
-        else
-        {
-            imageView1.animate().alpha(0).setDuration(2000);
-            imageView.animate().alpha(1).setDuration(2000);
-            count=0;
-        }
+        ImageView imageView=(ImageView) findViewById(R.id.imageView);
+            imageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(1000);
+           // imageView1.animate().alpha(1).setDuration(2000);
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        ImageView imageView=(ImageView) findViewById(R.id.imageView);
+        imageView.setX(-1000);
+        imageView.animate().translationXBy(1000).rotation(3600).setDuration(2000);
+
     }
 }
