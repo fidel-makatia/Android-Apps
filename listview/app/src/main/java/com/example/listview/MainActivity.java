b14f4bb595a8406cc6a2b15c7a44836f39c2be48
Fidel Makatia <fidelmakatia@gmail.com>
2022-02-09 13:49:09 +0300

    Third commit

---
+++ listview/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView myListView=findViewById(R.id.myListView);
+
+        ArrayList<String> myFamily= new ArrayList<String>();
+        myFamily.add("Fidel");
+        myFamily.add("Cynthia");
+        myFamily.add("Beryl");
+        myFamily.add("Mum");
+        myFamily.add("Dad");
+        myFamily.add("Fidel");
+        myFamily.add("Cynthia");
+        myFamily.add("Beryl");
+        myFamily.add("Mum");
+        myFamily.add("Dad");
+        myFamily.add("Fidel");
+        myFamily.add("Cynthia");
+        myFamily.add("Beryl");
+        myFamily.add("Mum");
+        myFamily.add("Dad");
+        myFamily.add("Fidel");
+        myFamily.add("Cynthia");
+        myFamily.add("Beryl");
+        myFamily.add("Mum");
+        myFamily.add("Dad");
+
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,myFamily);
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Person Selected", myFamily.get(position));
+
+                Toast.makeText(getApplicationContext(), "Hello "+myFamily.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
